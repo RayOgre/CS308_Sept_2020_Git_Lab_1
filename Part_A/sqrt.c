d7808b38373299624972381324c7d669dbe5784d
fixingSegmentation <mudit.rajesh@gmail.com>
2020-10-13 01:44:02 +0530

    modified:   sqrt.c 	Checking input and verifying that it is a valid number.

--- Part_A/sqrt.c
+++ Part_A/sqrt.c
@@ -4,6 +4,8 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 
 // The main function starts here.
 int main(int argc, char* argv[]) {
@@ -11,6 +13,15 @@ int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		printf("Usage: ./sqrt input\n");
 		exit(-1);
+	}
+
+	// Checking whether input is a number or not.
+	int len = strlen(argv[1]);
+	for (int i = 0; i<len; i++) {
+		if (!isdigit(argv[1][i])) {
+			printf("Input is not a valid number.\n");
+			exit(-1);
+		}
 	}
 
 	int input = atoi(argv[1]);
