a7545e7e047068cf6c8343ac04ae0bc8c0ffd5a8
fixingSegmentation <mudit.rajesh@gmail.com>
2020-10-13 01:54:54 +0530

    modified:   sqrt.c 	Added prefix 'NEGSUPPORT:' to every printf statement.

--- Part_A/sqrt.c
+++ Part_A/sqrt.c
@@ -11,23 +11,23 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: ./sqrt input\n");
+		printf("NEGSUPPORT:Usage: ./sqrt input\n");
 		exit(-1);
 	}
 	
 	// Checking whether input is a number or not.
 	int len = strlen(argv[1]);
 	if (len==1 && argv[1][0]=='-') {
-		printf("Input is not a valid number.\n");
+		printf("NEGSUPPORT:Input is not a valid number.\n");
 		exit(-1);
 	}
 	if (!(argv[1][0]=='-' && len>1) && !argv[1][0]) {
-		printf("Input is not a valid number.\n");
+		printf("NEGSUPPORT:Input is not a valid number.\n");
 		exit(-1);
 	}
 	for (int i = 1; i<len; i++) {
 		if (!isdigit(argv[1][i])) {
-			printf("Input is not a valid number.\n");
+			printf("NEGSUPPORT:Input is not a valid number.\n");
 			exit(-1);
 		}
 	}
@@ -35,12 +35,12 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	// Handling negative numbers
 	if (input<0) {
-		printf("Sqrt of %d is %fi\n",input,sqrt(-1*input));
+		printf("NEGSUPPORT:Sqrt of %d is %fi\n",input,sqrt(-1*input));
 	}
 	else {
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
+		printf("NEGSUPPORT:Sqrt of %d is %f\n",input,sqrt(input));
 	}
-	printf("End of program. Exiting.\n");
+	printf("NEGSUPPORT:End of program. Exiting.\n");
 	return(0);
 
 } // end main
